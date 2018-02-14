6a62a9bca7e86d98fba07d45420f3ccf28b575e7
nicolas.dorier <nicolas.dorier@gmail.com>
2018-02-14 22:06:41 +0900

    fix lightning-show

--- lightning-show.sh
+++ lightning-show.sh
@@ -1,3 +1,3 @@
 export CURRENT_HOST=`cat $LIGHTNING_ENV_FILE | sed -n 's/^CHARGED_HOST=\(.*\)$/\1/p'`
-export API_TOKEN=`cat $LIGHTNING_ENV_FILE | sed -n 's/^CHARGED_API_TOKEN=\(.*\)$/\1/p'`
+export CHARGED_API_TOKEN=`cat $LIGHTNING_ENV_FILE | sed -n 's/^CHARGED_API_TOKEN=\(.*\)$/\1/p'`
 echo "https://$CHARGED_API_TOKEN@$CURRENT_HOST/"
